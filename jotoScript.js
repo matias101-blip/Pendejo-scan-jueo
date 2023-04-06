cd78429eec7c3e96c2940d2cecaa83c8192e82f2
TheHunter101 <thehunter10121@gmail.com>
2023-04-06 15:49:41 -0500

    Full

--- jotoScript.js
+++ jotoScript.js
@@ -17,12 +17,108 @@ var imagenes = [
       respuesta: "La respuesta de la imagen 3 es: Nisekoi"
     },
     {
-        src: "img/3.jpg",
-        respuesta: "La respuesta de la imagen 2 es: SAILOR MOON"
+      src: "img/Akamegakill.jpg",
+      respuesta: "La respuesta de la imagen 4 es: Akame Ga Kill"
     },
     {
-        src: "img/nier.png",
-        respuesta: "La respuesta de la imagen 2 es: NIER AUTOMATA"
+        src: "img/SNK.png",
+        respuesta: "La respuesta de la imagen 5 es: Shingeki No Kyojin"
+    },
+    {
+      src: "img/Jujutsu-Kaisen.png",
+      respuesta: "La respuesta de la imagen 6 es: Jujutsu Kaisen"
+    },
+    {
+      src: "img/Hunter-x-Hunter.png",
+      respuesta: "La respuesta de la imagen 7 es: Hunter x Hunter"
+    },
+    {
+      src: "img/Cowboy Bebop.png",
+      respuesta: "La respuesta de la imagen 8 es: Cowboy Bebop"
+    },
+    {
+      src: "img/Assassination Classroom.png",
+      respuesta: "La respuesta de la imagen 9 es: Assassination Classroom"
+    },
+    {
+      src: "img/Deadman Wonderland.png",
+      respuesta: "La respuesta de la imagen 10 es: Deadman Wonderland"
+    },
+    {
+      src: "img/Death Note.png",
+      respuesta: "La respuesta de la imagen 11 es: Death Note"
+    },
+    {
+      src: "img/Demon Slayer.png",
+      respuesta: "La respuesta de la imagen 12 es: Demon Slayer"
+    },
+    {
+      src: "img/Dragon Ball Z.png",
+      respuesta: "La respuesta de la imagen 13 es: Dragon Ball Z"
+    },
+    {
+      src: "img/Fairy Tail.png",
+      respuesta: "La respuesta de la imagen 14 es: Fairy Tail"
+    },
+    {
+      src: "img/Fruits Basket.png",
+      respuesta: "La respuesta de la imagen 15 es: Fruits Basket"
+    },
+    {
+      src: "img/Fullmetal Alchemist Brotherhood.png",
+      respuesta: "La respuesta de la imagen 16 es: Fullmetal Alchemist Brotherhood"
+    },
+    {
+      src: "img/Kimi no Na Wa.png",
+      respuesta: "La respuesta de la imgen 17 es: Kimi no Na Wa"
+    },
+    {
+      src: "img/Naruto.png",
+      respuesta: "La respuesta de la imagen 18 es: Naruto"
+    },
+    {
+      src: "img/Neon Genesis Evangelion.png",
+      respuesta: "La respuesta de la imagen 19 es: Neon Genesis Evangelion"
+    },
+    {
+      src: "img/One Piece.png",
+      respuesta: "La respuesta de la imagen 20 es: One Piece"
+    },
+    {
+      src: "img/Quintessential Quintuplets.png",
+      respuesta: "La respuesta de la imagen 21 es: Quintessential Quintuplets"
+    },
+    {
+      src: "img/Shingatsu wa Kimi no Uso.png",
+      respuesta: "La respuesta de la imagen 22 es: Shingatsu wa Kimi no Uso"
+    },
+    {
+      src: "img/Soul Eater.png",
+      respuesta: "La respuesta de la imagen 23 es: Soul Eater"
+    },
+    {
+      src: "img/Sword Art Online.png",
+      respuesta: "La respuesta de la imagen 24 es: Sword Art Online"
+    },
+    {
+      src: "img/The Promised Neverland.png",
+      respuesta: "La respuesta de la imagen 25 es: The Promised Neverland"
+    },
+    {
+      src: "img/Tokyo Ghoul.png",
+      respuesta: "La respuesta de la imagen 26 es: Tokyo Ghoul"
+    },
+    {
+      src: "img/Toradora.png",
+      respuesta: "La respuesta de la imagen 27 es: Toradora"
+    },
+    {
+      src: "img/Yuri on Ice!!.png",
+      respuesta: "La respuesta de la imagen 28 es: Yuri on ice!!"
+    },
+    {
+      src: "img/One Punch Man.png",
+      respuesta: "La respuesta de la imagen 29 es: One Punch Man"
     },
     {
         src: "img/ojos.jpg",
